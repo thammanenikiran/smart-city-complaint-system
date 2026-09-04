3fa185246d4c78b2e4face97c4d19e8f8711bb76
kiran <kk1393605@gmail.com>
2026-09-04 21:59:40 +0530

    Fix application homepage

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,16 +1,8 @@
-from flask import Blueprint
-from sqlalchemy import text
-
-from database.db import db
+from flask import Blueprint, redirect, url_for
 
 main_bp = Blueprint("main", __name__)
 
 
 @main_bp.route("/")
 def home():
-    try:
-        db.session.execute(text("SELECT 1"))
-        return "Flask + MySQL connection successful!"
-
-    except Exception as e:
-        return f"Database connection failed: {e}"
+    return redirect(url_for("auth.login"))
